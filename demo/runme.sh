f8ebdca128ce6b550d090b45dae7ef43d54d00e1
yeezhu <zhu.yee@outlook.com>
2018-01-04 15:03:30 +0800

    minor changes

--- demo/runme.sh
+++ demo/runme.sh
@@ -3,5 +3,5 @@
 export CUDA_VISIBLE_DEVICES=0,1
 # train
 # ../data/voc/ is the path of VOCdevkit.
-python -m experiment.demo_voc2007 /root/Cloud/data/voc/ \
+python -m experiment.demo_voc2007 ../data/voc/ \
 --image-size 112,224,560 --batch-size 16 --lr 0.01 --epochs 20 
